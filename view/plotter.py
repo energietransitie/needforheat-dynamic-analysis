f224a7887a81d50064d1caa308a898c84a2835fd
Henri ter Hofte <h.ter.hofte@windesheim.nl>
2022-10-24 19:39:42 +0000

    update plotter: use dotted markers, no lines

--- view/plotter.py
+++ view/plotter.py
@@ -31,6 +31,9 @@ class Plot:
         #add DataFrames to subplots
         df[temp_plot_dict.keys()].plot(
             ax=ax[0],
+            marker=".",
+            ms=2,
+            linestyle='None',
             secondary_y=temp_plot_2nd_list,
             grid=True, 
             legend=True, 
@@ -45,6 +48,9 @@ class Plot:
         df[power_plot_dict.keys()].plot(
             ax=ax[1],
             secondary_y=power_plot_2nd_list, 
+            marker=".",
+            ms=2,
+            linestyle='None',
             mark_right=True, 
             grid=True, 
             legend=True, 
@@ -69,7 +75,14 @@ class Plot:
                                    power_plot_2nd_list = [],
                                    power_2nd_y_label = r'$I\ [W/m^2]$'):
         
-        for home_id in df.index.levels[0]:
+
+        # for home_id in df.index.levels[0]:
+        ## line above  did not work in specific cases, 
+        ## e.g. when df = df_results_tempsim.query('home_id == 809743')
+
+        ##  so we found a workaround:
+        homes_to_plot = df.index.to_frame(index=False).home_id.unique()
+        for home_id in homes_to_plot:
             Plot.temperature_and_power_one_home_plot(f'Learned model parameters for home: {home_id}',
                                                      df.loc[home_id],
                                                      shared_x_label = shared_x_label,
@@ -86,7 +99,6 @@ class Plot:
     @staticmethod
     def temperature_and_power_one_home_weekly_plot(home_id:int,
                                                    df: pd.DataFrame,
-                                                   sanity_threshold = 0.9,
                                                    shared_x_label='Datetime',
                                                    temp_plot_dict = {},
                                                    temp_y_label = r'$Temperature [^oC]$',
@@ -99,6 +111,7 @@ class Plot:
 
         first_day = df.index.min()
         last_day = df.index.max()
+        
         for moving_horizon_start in pd.date_range(start=first_day, end=last_day, inclusive='left', freq='7D'):
             moving_horizon_end = min(last_day, moving_horizon_start + timedelta(days=7))
 
@@ -110,19 +123,18 @@ class Plot:
             moving_horizon_end = df_moving_horizon.index.max()
 
 
-            print(f'Learned model parameters for home: {home_id} from {moving_horizon_start} to {moving_horizon_end} with sanity {df_moving_horizon.sanity.astype(float).mean():.2f}')
-            if (df_moving_horizon.sanity.astype(float).mean() > sanity_threshold):
-                Plot.temperature_and_power_one_home_plot(f'Learned model parameters for home: {home_id} from {moving_horizon_start} to {moving_horizon_end}',
-                                                         df_moving_horizon,                                                         
-                                                         shared_x_label = shared_x_label,
-                                                         temp_plot_dict = temp_plot_dict,
-                                                         temp_y_label = temp_y_label,
-                                                         temp_plot_2nd_list = temp_plot_2nd_list,
-                                                         temp_2nd_y_label = temp_2nd_y_label,
-                                                         power_plot_dict = power_plot_dict,
-                                                         power_y_label = power_y_label,
-                                                         power_plot_2nd_list = power_plot_2nd_list,
-                                                         power_2nd_y_label = power_2nd_y_label)
+            print(f'Learned model parameters for home: {home_id} from {moving_horizon_start} to {moving_horizon_end}')
+            Plot.temperature_and_power_one_home_plot(f'Learned model parameters for home: {home_id} from {moving_horizon_start} to {moving_horizon_end}',
+                                                     df_moving_horizon,
+                                                     shared_x_label = shared_x_label,
+                                                     temp_plot_dict = temp_plot_dict,
+                                                     temp_y_label = temp_y_label,
+                                                     temp_plot_2nd_list = temp_plot_2nd_list,
+                                                     temp_2nd_y_label = temp_2nd_y_label,
+                                                     power_plot_dict = power_plot_dict,
+                                                     power_y_label = power_y_label,
+                                                     power_plot_2nd_list = power_plot_2nd_list,
+                                                     power_2nd_y_label = power_2nd_y_label)
             
             
             
