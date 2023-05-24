8339b92ea9acc843f98b285146d43de08c02994f
HosseinRahmani <h.rahmani@windesheim.nl>
2023-05-24 15:04:28 +0200

    Add Scatterplot
    1- scatterplot function added in plotter.py
    2- scatterplot function called in "B4B_analysis_real_ds.ipynb" and "B4B_analysis_virtual_ds.ipynb"

--- view/plotter.py
+++ view/plotter.py
@@ -308,3 +308,18 @@ class Plot:
                    rot=35,
                    title=parameter)
             )
+            
+    @staticmethod
+    def learned_parameters_boxplot_b4b(df_results_model_parameters: pd.DataFrame, parameter: str):
+        """
+        Visualize results of all learned model parameters of all homes in one box plot
+        """
+        df_results_model_parameters.boxplot(by='id', column=parameter)
+        
+        
+    @staticmethod
+    def learned_parameters_scatterplot(df: pd.DataFrame, parameters: list):
+        """
+        Visualize results of all learned model parameters of all homes in one box plot
+        """
+        df.plot.scatter(parameters[0], parameters[1])
